c13d1ed8c80d40a22202b9265aa2a0645b1c158f
Hadia7211 <Hadiafaisal7211@gmail.com>
2026-07-01 22:08:40 +0500

    Initial React Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>SehatSafe</h1>
+      <p>Privacy-aware SRH chatbot prototype</p>
+    </div>
+  );
+}
+
+export default App;
